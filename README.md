ad309ee0b6bc2c7f78c8db9b6b9d85a73aa56174
calvin <calvinwyoung@gmail.com>
2011-03-07 12:27:08 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+Minno Python SDK
+================
+
+This is the basic Python SDK for synchronous server-side integration with Minno.
+
+For more information, visit our [API Documentation page](https://www.minno.co/docs).
+
+Usage
+-----
+
+This SDK contains a single function `is_purchase_valid()` that returns a boolean
+indicating whether the purchase was valid. To start, here is an example of its
+server-side usage:
+
+    from project.path import minno
+
+    # Receive these POST variables from your minnoCallback() function
+    user_id = request.params["userId"]
+    invitem_id = request.params["invitemId"]
+
+    # Fill this in with your partner verification token. You can get this from
+    # the "Settings" tab in the Partner Dashboard
+    verif_token = xxx
+
+    if minno.is_purchase_valid(user_id, invitem_id, verif_token):
+        # Deliver the purchased item to your user!
+    else:
+        # Oops, the user hasn't purchased this item!
+
+You receive the `userId` and `invitemId` from the client-side `minnoCallback()`
+function that gets executed after a user purchases an item. You can then POST
+these parameters to your server endpoint for verification before giving the user
+access to your premium service.
+
+The implementation of the verification is very simple--it uses Python's urllib2
+module to perform a synchronous GET request to our purchase-verification
+endpoint. This can be fairly easily adapted for those running asynchronous web
+servers if needed.
+
+Feedback
+--------
+
+If you have any questions or comments about this package, feel free to email us
+at support@minno.co! We'd love to hear your thoughts!
---
+++ src/minno.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#
+# Copyright 2010 Minno
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+import urllib2
+
+def is_purchase_valid(user_id, invitem_id, verif_token):
+    """Returns a boolean indicating whether the purchase was valid.
+
+    Performs a synchronous GET request to the Minno purchase verification
+    endpoint.
+
+    Args:
+        user_id, string: the user whose purchase we are validating
+        invitem_id, string: the id of the inventory item the user purchased
+        verif_token, string: your partner verification token, which can be found
+          in the "Settings" tab of the Partner Dashboard
+
+    Exceptions:
+        InvalidVerifTokenException: raised if the given verifToken is invalid
+
+    Returns:
+        bool: True of the user purchased the item, False otherwise
+    """
+    purchase_status = urllib2.urlopen(
+        "https://www.minno.co/p/%s/%s?verifToken=%s"
+        % (user_id, invitem_id, verif_token))
+
+    try:
+        json_status = json.loads(purchase_status.read())
+        return json_status["isValid"]
+    except ValueError:
+        raise InvalidVerifTokenException()
+
+class InvalidVerifTokenException(Exception):
+    pass
