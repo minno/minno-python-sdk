41439e881df1120bce861f5e43d07d83a5ee4142
calvin <calvinwyoung@gmail.com>
2011-03-08 20:05:42 -0800

    Added author to sdk source

--- src/minno.py
+++ src/minno.py
@@ -13,6 +13,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+#
+# Author: Calvin Young <calvin@minno.co>
 
 import json
 import urllib2
